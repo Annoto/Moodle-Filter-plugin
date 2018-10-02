8f8c4e51e2a65e82fd7833feb0b325d49e554c73
Hen Eytan <hen.eytan@gmail.com>
2018-10-02 18:37:06 +0300

    Updated to the requirerd minimal version of Moodle where this plugin was tested and found working - version 3.1

--- version.php
+++ version.php
@@ -26,5 +26,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018082600;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2014051200;        // Requires this Moodle version 2.7.
+$plugin->requires  = 2016052300;        // Requires this Moodle version 3.1.
 $plugin->component = 'filter_annoto';  // Full name of the plugin (used for diagnostics).
